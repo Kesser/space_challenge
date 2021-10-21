5536a60622f944204007ac68bb61b9f5279193a4
Kesser <kesserm@gmail.com>
2021-10-21 17:35:21 +0000

    sort astronaut names alphabetically.

--- lib/astronaut_service.rb
+++ lib/astronaut_service.rb
@@ -19,8 +19,23 @@ class AstronautService
       people_in_space[craft_name] ||= []
       people_in_space[craft_name] << person["name"]
     end
+    people_in_space.each do |craft_name, people|
+      people_in_space[craft_name] = people.sort {|person1, person2|
+        last_name1 = get_last_name(person1)
+        last_name2 = get_last_name(person2)
+        last_name1 <=> last_name2
+      }
+    end
     people_in_space
   end
+
+  def get_last_name(name)
+    names = name.split(' ')
+    return '' if names.length == 1
+    return names.last if names.length == 2
+    return names[1..names.length].join(' ') if names.length >= 3
+  end
+
 
   def count_astronauts
     @data["number"]
